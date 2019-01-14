c392daa9305e58400a6ab2b5138e66e39813b11c
Reza Ambler <reza.ambler@gmail.com>
2019-01-14 07:24:22 -0800

    updates

--- Windows/scripts/Install-PowerShell5.ps1
+++ Windows/scripts/Install-PowerShell5.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop"
 
 if ($PSVersionTable.PSVersion.Major -le 4) {
     Write-Host "==> Installing Newer Version of PowerShell"
-    choco install powershell -y
+    choco install powershell -y --no-progress
   } else {
     Write-Host "==> Not installing PowerShell as it is 5+"
   }

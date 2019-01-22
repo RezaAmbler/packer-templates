5a60cab57e3c7dd1872241ea677f70100fa076a5
Reza Ambler <reza.ambler@gmail.com>
2019-01-22 15:42:52 -0800

    proxy stuff
    add some logic to go through a proxy -- may not need it due to Set-InternetProxy.ps1 -- testing continues

--- Windows/scripts/Install-Chocolatey.ps1
+++ Windows/scripts/Install-Chocolatey.ps1
@@ -1,7 +1,13 @@
 $ProgressPreference="SilentlyContinue"
 $ErrorActionPreference = "stop"
 $env:chocolateyUseWindowsCompression = 'false'
+$env:chocolateyProxyLocation = $env:proxy_string
 
-
-write-output "==> Installing chocolatey"
-Invoke-WebRequest -Uri 'https://chocolatey.org/install.ps1' -UseBasicParsing | Invoke-Expression
+if ($env:proxy_enabled -contains 'true') {
+    Write-Output "==> Installing chocolatey using Squid $env:proxy_string"
+#    $env:chocolateyProxyLocation = 'http://us1rhprox01.corp.resmed.org:32611'
+    Invoke-WebRequest -Proxy "$env:proxy_string" -Uri 'https://chocolatey.org/install.ps1' -UseBasicParsing | Invoke-Expression
+  } Else {
+    Write-Output "==> Installing chocolatey without proxy"
+    Invoke-WebRequest -Uri 'https://chocolatey.org/install.ps1' -UseBasicParsing | Invoke-Expression
+  }
--- Windows/scripts/Install-PowerShell5.ps1
+++ Windows/scripts/Install-PowerShell5.ps1
@@ -1,9 +1,17 @@
 $ProgressPreference="SilentlyContinue"
 $ErrorActionPreference = "Stop"
 
+
 if ($PSVersionTable.PSVersion.Major -le 4) {
-    Write-Host "==> Installing Newer Version of PowerShell"
-    choco install powershell -y --no-progress
+    if ($env:proxy_enabled -contains 'true') {
+      Write-Host "==> Installing Newer Version of PowerShell"
+      $env:chocolateyProxyLocation = $env:proxy_string
+      choco install powershell -y --no-progress
+    }
+    else {
+      Write-Host "==> Installing Newer Version of PowerShell"
+      choco install powershell -y --no-progress 
+    }
   } else {
     Write-Host "==> Not installing PowerShell as it is 5+"
   }

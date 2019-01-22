516afa0d7d5cd0991cec7dda552e24db2108eda7
Reza Ambler <reza.ambler@gmail.com>
2019-01-22 15:59:09 -0800

    choco install updates
    updates

--- Windows/scripts/Install-Chocolatey.ps1
+++ Windows/scripts/Install-Chocolatey.ps1
@@ -1,12 +1,12 @@
 $ProgressPreference="SilentlyContinue"
 $ErrorActionPreference = "stop"
 $env:chocolateyUseWindowsCompression = 'false'
-$env:chocolateyProxyLocation = $env:proxy_string
+$env:chocolateyProxyLocation = $env:http_proxy
 
 if ($env:proxy_enabled -contains 'true') {
-    Write-Output "==> Installing chocolatey using Squid $env:proxy_string"
+    Write-Output "==> Installing chocolatey using Squid $env:http_proxy"
 #    $env:chocolateyProxyLocation = 'http://us1rhprox01.corp.resmed.org:32611'
-    Invoke-WebRequest -Proxy "$env:proxy_string" -Uri 'https://chocolatey.org/install.ps1' -UseBasicParsing | Invoke-Expression
+    Invoke-WebRequest -Proxy "$env:http_proxy" -Uri 'https://chocolatey.org/install.ps1' -UseBasicParsing | Invoke-Expression
   } Else {
     Write-Output "==> Installing chocolatey without proxy"
     Invoke-WebRequest -Uri 'https://chocolatey.org/install.ps1' -UseBasicParsing | Invoke-Expression

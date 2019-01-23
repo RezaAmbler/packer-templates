ee88013c6e40333cf7cace3b3d132c7bd86029e1
Brian Warlick <thunderbump@gmail.com>
2019-01-23 14:43:09 -0800

    exports to vsphere and uploads to s3
    requires packer version from https://github.com/hashicorp/packer/issues/6742

--- Windows/scripts/Install-Chocolatey.ps1
+++ Windows/scripts/Install-Chocolatey.ps1
@@ -2,6 +2,6 @@ $ProgressPreference="SilentlyContinue"
 $ErrorActionPreference = "stop"
 $env:chocolateyUseWindowsCompression = 'false'
 
-
+ping -n 1 google.com
 write-output "==> Installing chocolatey"
 Invoke-WebRequest -Uri 'https://chocolatey.org/install.ps1' -UseBasicParsing | Invoke-Expression
